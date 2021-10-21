dfe580f958a06d9c014d88b9ffc9da3657e604de
LukeChemeriov <dgm.bailey@outlook.com>
2021-10-21 12:49:21 +0100

    Initial comit of class function

---
+++ hack01/class.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+class hack01 {
+// private, class only functions
+private:
+	// kprintf - print things to the screen
+	void kprintf(const char* input = "[ERROR]: No text specified!", int EndLine = 0) {
+		if (EndLine == 0) {
+			std::cout << input << std::endl;
+		}
+		else {
+			std::cout << input;
+		}
+	}
+	// kscanf - get input from the user, with a designated prompt
+	std::string kscanf(const char* prompt) {
+		kprintf(prompt, 1);
+		std::string input;
+		std::cin >> input;
+		return input;
+	}
+// constructor and destructor
+public:
+
+	hack01() {
+		// Generate UI
+		kprintf("hack01");
+		
+	};
+
+};
