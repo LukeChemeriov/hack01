3247e8f5e61b10fd64e7e29d0a41ed881263b4fa
LukeChemeriov <dgm.bailey@outlook.com>
2021-10-21 13:18:51 +0100

    Started easy mode

--- hack01/class.cpp
+++ hack01/class.cpp
@@ -3,7 +3,7 @@ class hack01 {
 // private, class only functions
 private:
 	// kprintf - print things to the screen
-	void kprintf(const char* input = "[ERROR]: No text specified!", int EndLine = 0) {
+	void kprintf(std::string input = "[ERROR]: No text specified!", int EndLine = 0) {
 		if (EndLine == 0) {
 			std::cout << input << std::endl;
 		}
@@ -30,6 +30,34 @@ public:
 // main functions
 public:
 	void HackSimWithEasyDifficulty() {
+
+		kprintf("Entering terminal.... ", 1);
+		kprintf("done");
+		kprintf("\n\n\n[HINT]: Start by creating a connection to a server of your choice.");
+		kprintf("\tYou can use the command - INIT connection\"\"");
+		std::string input;
+		input = kscanf(">>>");
+		input = kscanf("Enter TARGET IP >>>");
+		std::string hackingIP = input;
+		kprintf("Connected to main server" + hackingIP);
+		kprintf("[HINT]: Use the ACCESS command to access a certain folder!");
+		kprintf("\tYou can use - ACCESS folder");
+		input = kscanf(">>>");
+		input = kscanf("Enter folder NAME: >>>");
+		kprintf("ACCESS DENIED: ERRNO 42662 - AUTHORIZATION REQUIRED!");
+		kprintf("[HINT]: Try overriding the security e!\n\tUse - override security settings");
+		kprintf("ACCESS DENIED: ERRNO 42665 - LOGIN REQUIRED!");
+		kprintf("[HINT]: Try listing the admin users!\n\tUse - list admin users");
+		input = kscanf(">>>");
+		kprintf("RESPONSE FROM SERVER: [nassadmin], [bidengov]");
+		kprintf("[HINT]: Crack the password using auth_crack.exe");
+		kprintf("\tLAUNCH auth_crack.exe\n\tactivate mode crack\n\taccess user [nassadmin] at [" + hackingIP +"]\n\tRUN password crack");
+		input = kscanf(">>>");
+		input = kscanf(">>>");
+		input = kscanf(">>>");
+		input = kscanf(">>>");
+		kprintf("RESPONSE FROM SERVER: Server busy");
+		kprintf("");
 
 	}
 	void HackSimWithMediumDifficulty() {
